a049cde894befcf286479ac892b6cea7181867a2
mascot6699 <umangshucool@gmail.com>
2017-06-05 17:04:28 +0530

    Adds fallback to get windows size

--- Learning.md
+++ Learning.md
@@ -11,3 +11,6 @@ We exit the program with an exit status of 1, which indicates failure (as would 
 Character in lower or capital when anded with 31 give 1-based index from a. `ord("a") & 31` == `ord("a") & 31` == 1.
 An escape sequence to the terminal. Escape sequences always start with an escape character `\x1b(27)` followed by a `[` character.
 Size of the terminal by simply calling `ioctl()` with the `TIOCGWINSZ(short for Terminal Input/Output Control Get WINdow SiZe)` request.
+The C command (Cursor Forward) moves the cursor to the right.
+The B command (Cursor Down) moves the cursor down.
+The n command (Device Status Report) can be used to query the terminal for status information.
--- code-scorpion.c
+++ code-scorpion.c
@@ -1,12 +1,12 @@
 /*** includes ***/
 
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/ioctl.h>
 #include <termios.h>
-#include <errno.h>
+#include <unistd.h>
 
 /*** defines ***/
 
@@ -27,24 +27,24 @@ struct editorConfig E;
 void die(const char *s) {
   write(STDOUT_FILENO, "\x1b[2J", 4);
   write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
 
 void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
-    	die("tcsetattr");
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
+    die("tcsetattr");
 }
 
 void enableRawMode() {
-
   if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
   atexit(disableRawMode);
 
   struct termios raw = E.orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-  raw.c_iflag &= ~(ICRNL | IXON);
   raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
@@ -61,10 +61,31 @@ char editorReadKey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+  return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
+
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    return getCursorPosition(rows, cols);
   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
@@ -77,7 +98,11 @@ int getWindowSize(int *rows, int *cols) {
 void editorDrawRows() {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    write(STDOUT_FILENO, "~", 1);
+
+    if (y < E.screenrows - 1) {
+      write(STDOUT_FILENO, "\r\n", 2);
+    }
   }
 }
 
@@ -86,6 +111,7 @@ void editorRefreshScreen() {
   write(STDOUT_FILENO, "\x1b[H", 3);
 
   editorDrawRows();
+
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
@@ -93,6 +119,7 @@ void editorRefreshScreen() {
 
 void editorProcessKeypress() {
   char c = editorReadKey();
+
   switch (c) {
     case CTRL_KEY('q'):
       write(STDOUT_FILENO, "\x1b[2J", 4);
